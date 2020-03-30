d3ff37049080c741c2fb9b3480f9fae607453654
Enrique González <enrique.gonzalez@speakingathome.com>
2020-03-30 11:53:45 +0200

    first commit

---
+++ pages/lazy-page-with-default-layout.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="lazy">
+        Lazy page
+    </div>
+</template>
+
+<script>
+    export default {
+        layout: 'lazy-layout'
+    }
+</script>
+
+<style lang="scss" scoped>
+    .lazy {
+        color: blue;
+    }
+</style>
