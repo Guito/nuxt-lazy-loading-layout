04f1a776e830dcd3783147fe9a088d00a7f2aa36
Enrique González <enrique.gonzalez@speakingathome.com>
2020-03-30 11:58:35 +0200

    Adding moment timezone

--- layouts/lazy-layout.vue
+++ layouts/lazy-layout.vue
@@ -5,6 +5,17 @@
     </div>
 </template>
 
+<script>
+    let moment = () => import('moment-timezone'); // Improved performance
+    const created = async () => {
+        moment = await moment();
+    }
+    created()
+
+    export default {
+    }
+</script>
+
 <style lang="scss" scoped>
     .test-style {
         color: red;
